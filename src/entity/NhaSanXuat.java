8db75d539bdffaf1be7fdca2e69689bfc86419e9
DESKTOP-9TGIV6T\DELL <trongtan123tan@gmail.com>
2025-04-20 16:05:12 +0700

    Initial commit

--- src/entity/NhaSanXuat.java
+++ src/entity/NhaSanXuat.java
@@ -1,4 +1,59 @@
 package entity;
 
 public class NhaSanXuat {
+    private String maNhaSanXuat;
+    private String tenNhaSanXuat;
+    private String quocGia;
+    private String diaChi;
+
+    public NhaSanXuat(String maNhaSanXuat, String tenNhaSanXuat, String quocGia, String diaChi) {
+        this.maNhaSanXuat = maNhaSanXuat;
+        this.tenNhaSanXuat = tenNhaSanXuat;
+        this.quocGia = quocGia;
+        this.diaChi = diaChi;
+    }
+
+    public NhaSanXuat(String maNhaSanXuat) {
+        this.maNhaSanXuat = maNhaSanXuat;
+    }
+
+    public String getMaNhaSanXuat() {
+        return maNhaSanXuat;
+    }
+
+    public void setMaNhaSanXuat(String maNhaSanXuat) {
+        this.maNhaSanXuat = maNhaSanXuat;
+    }
+
+    public String getTenNhaSanXuat() {
+        return tenNhaSanXuat;
+    }
+    public void setTenNhaSanXuat(String tenNhaSanXuat) {
+        this.tenNhaSanXuat = tenNhaSanXuat;
+    }
+
+    public String getQuocGia() {
+        return quocGia;
+    }
+    public void setQuocGia(String quocGia) {
+        this.quocGia = quocGia;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    @Override
+    public String toString() {
+        return "NhaSanXuat{" +
+                "maNhaSanXuat='" + maNhaSanXuat + '\'' +
+                ", tenNhaSanXuat='" + tenNhaSanXuat + '\'' +
+                ", quocGia='" + quocGia + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                '}';
+    }
 }
--- src/entity/TheLoai.java
+++ src/entity/TheLoai.java
@@ -1,4 +1,47 @@
 package entity;
 
 public class TheLoai {
+    private String maTheLoai;
+    private String tenTheLoai;
+    private String moTa;
+
+    public TheLoai(String maTheLoai, String tenTheLoai, String moTa) {
+        this.maTheLoai = maTheLoai;
+        this.tenTheLoai = tenTheLoai;
+        this.moTa = moTa;
+    }
+
+    public TheLoai(String maTheLoai) {
+        this.maTheLoai = maTheLoai;
+    }
+
+    public String getMaTheLoai() {
+        return maTheLoai;
+    }
+
+    public void setMaTheLoai(String maTheLoai) {
+        this.maTheLoai = maTheLoai;
+    }
+
+    public String getTenTheLoai() {
+        return tenTheLoai;
+    }
+
+    public void setTenTheLoai(String tenTheLoai) {
+        this.tenTheLoai = tenTheLoai;
+    }
+    public String getMoTa() {
+        return moTa;
+    }
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    @Override
+    public String toString() {
+        return "TheLoai{" +
+                "maTheLoai='" + maTheLoai + '\'' +
+                ", tenTheLoai='" + tenTheLoai + '\'' +
+                '}';
+    }
 }
