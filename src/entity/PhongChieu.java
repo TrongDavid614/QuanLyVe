17be1684017b86cc109577c03007a7750dfc7483
DangPhanHoang <122417704+DangPhanHoang@users.noreply.github.com>
2025-04-21 01:05:10 +0700

    Update PhongChieu.java

--- src/entity/PhongChieu.java
+++ src/entity/PhongChieu.java
@@ -19,12 +19,40 @@ public class PhongChieu {
         this.trangThai = trangThai;
     }
     
-    public int getMaPhongChieu() {
-        return maPhongChieu;
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhongChieu {
+    private int maPhong;
+    private String tenPhong;
+    private int soGhe;
+    private List<Ghe> danhSachGhe;
+    
+    public PhongChieu() {
+        this.danhSachGhe = new ArrayList<>();
     }
     
-    public void setMaPhongChieu(int maPhongChieu) {
-        this.maPhongChieu = maPhongChieu;
+    public PhongChieu(String tenPhong, int soGhe) {
+        this.tenPhong = tenPhong;
+        this.soGhe = soGhe;
+        this.danhSachGhe = new ArrayList<>();
+    }
+    
+    public PhongChieu(int maPhong, String tenPhong, int soGhe) {
+        this.maPhong = maPhong;
+        this.tenPhong = tenPhong;
+        this.soGhe = soGhe;
+        this.danhSachGhe = new ArrayList<>();
+    }
+    
+    public int getMaPhong() {
+        return maPhong;
+    }
+    
+    public void setMaPhong(int maPhong) {
+        this.maPhong = maPhong;
     }
     
     public String getTenPhong() {
@@ -35,42 +63,57 @@ public class PhongChieu {
         this.tenPhong = tenPhong;
     }
     
-    public int getSucChua() {
-        return sucChua;
+    public int getSoGhe() {
+        return soGhe;
     }
     
-    public void setSucChua(int sucChua) {
-        this.sucChua = sucChua;
+    public void setSoGhe(int soGhe) {
+        this.soGhe = soGhe;
     }
     
-    public String getLoaiPhong() {
-        return loaiPhong;
+    public List<Ghe> getDanhSachGhe() {
+        return danhSachGhe;
     }
     
-    public void setLoaiPhong(String loaiPhong) {
-        this.loaiPhong = loaiPhong;
+    public void setDanhSachGhe(List<Ghe> danhSachGhe) {
+        this.danhSachGhe = danhSachGhe;
     }
     
-    public boolean isTrangThai() {
-        return trangThai;
+    public void themGhe(Ghe ghe) {
+        if (this.danhSachGhe == null) {
+            this.danhSachGhe = new ArrayList<>();
+        }
+        danhSachGhe.add(ghe);
+        ghe.setPhongChieu(this);
     }
     
-    public void setTrangThai(boolean trangThai) {
-        this.trangThai = trangThai;
-    }
+    public void taoGheTuDong() {
+        int tongSoGhe = this.soGhe;
+        int soGheMotHang = 10; 
+        int soHang = (int) Math.ceil((double) tongSoGhe / soGheMotHang);
+        int phanTramGheVIP = 50; 
+        int soGheVIP = (tongSoGhe * phanTramGheVIP) / 100;
+        
+        char hangChu = 'A';
+        int gheDem = 0;
     
-    public void batHoatDong() {
-        this.trangThai = true;
-    }
+        for (int i = 0; i < soHang; i++) {
+            for (int j = 1; j <= soGheMotHang; j++) {
+                if (gheDem >= tongSoGhe) break;
+                
+                String tenGhe = String.valueOf(hangChu) + j;
+                String loaiGhe = (gheDem < soGheVIP) ? "VIP" : "Thường";
     
-    public void tatHoatDong() {
-        this.trangThai = false;
+                Ghe ghe = new Ghe(tenGhe, loaiGhe, this);
+                themGhe(ghe);
+                gheDem++;
+            }
+            hangChu++;
+        }
     }
     
     @Override
     public String toString() {
-        return "PhongChieu [maPhongChieu=" + maPhongChieu + ", tenPhong=" + tenPhong + 
-               ", sucChua=" + sucChua + ", loaiPhong=" + loaiPhong + 
-               ", trangThai=" + (trangThai ? "Đang hoạt động" : "Đang bảo trì") + "]";
+        return "PhongChieu [maPhong=" + maPhong + ", tenPhong=" + tenPhong + ", soGhe=" + soGhe + "]";
     }
 }
